44eeff55a6fdb4598d9cf5d5cf4a759acbfb366d
Techwrecker <72864121+Techwrecker@users.noreply.github.com>
2023-12-27 00:21:51 +0900

    Update activation.py

--- activation.py
+++ activation.py
@@ -2,12 +2,14 @@ import torch
 from torch import Tensor
 import torch.nn as nn
 
-
 class Sqrt(nn.Module):
 	def __init__(self, slope: float = 1.):
 		super().__init__()
 		self.slope = slope
+	
+	def sqrt(self, input_tensor: Tensor) -> Tensor:
+		self.result = torch.mul(torch.mul(torch.sub(torch.sqrt(torch.add(torch.abs(input_tensor), 0.25)), 0.5), torch.sgn(input_tensor)), self.slope)
+		return self.result
 
 	def forward(self, input_tensor: Tensor) -> Tensor:
-		slope_tensor = abs(input_tensor) / input_tensor
-		return ((abs(input_tensor) + 0.25) ** 0.5 - 0.5) * self.slope * slope_tensor
+		return self.sqrt(input_tensor)
